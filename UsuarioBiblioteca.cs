897f1eab12a4f665c2fc441f61148e7c40803b37
Moises Garcia <victorgpocas@gmail.com>
2025-09-14 21:04:35 -0600

    ya podemos ingresar una tabla de estudiantes

--- UsuarioBiblioteca.cs
+++ UsuarioBiblioteca.cs
@@ -4,11 +4,27 @@ namespace Biblioteca_Gestion2
 {
     public class UsuarioBiblioteca
     {
-        public object Nombre { get; internal set; }
+        private string text1;
+        private string text2;
 
+        public UsuarioBiblioteca(string text1, string text2)
+        {
+            this.text1 = text1;
+            this.text2 = text2;
+        }
+
+        public object Nombre { get; internal set; }
+        public object Carnet { get; internal set; }
+         
+
+
+
+        // Solución para CS0161: Se debe devolver un valor en todas las rutas de acceso.
+        // Solución para IDE0060: El parámetro 'v' no se usa, pero es necesario para la firma del operador implícito.
         public static implicit operator UsuarioBiblioteca(bool v)
         {
-            throw new NotImplementedException();
+            // Retorna un nuevo objeto UsuarioBiblioteca con valores predeterminados.
+            return new UsuarioBiblioteca(string.Empty, string.Empty);
         }
     }
 }
